79d5bf59b3c210f8932be04657493a1d52fd4f15
Ashish Suri <75773071+KnowAashish@users.noreply.github.com>
2023-10-11 16:12:39 -0400

    DFF_RTL

--- dff_rtl.sv
+++ dff_rtl.sv
@@ -1,17 +1,20 @@
-/*
-module dff (input  clk,
-			input  resetn,
-			input  din,
-			output dout);
-	
-	always @(posedge clk) begin
-		if(!resetn)
-			dout <= 1'b0;
-		else
-			dout <= din;
-	end
+module dff (input clk,
+            input resetn,
+            input din,
+            output logic dout);
+  
+  always @(posedge clk) begin
+    if(!resetn)
+      dout <= 1'b0;
+    else
+      if(din === 1'bx)
+        dout <= 1'b0;
+    else
+      dout <= din;
+  end
 endmodule
-*/
+
+/*
 module dff (intf vif_dut);
 
 	always @(posedge vif_dut.clk) begin
@@ -21,3 +24,4 @@ module dff (intf vif_dut);
 			vif_dut.dout <= vif_dut.din;
 	end
 endmodule
+*/
